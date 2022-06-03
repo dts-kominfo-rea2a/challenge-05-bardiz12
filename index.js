6b77bb2ba4365c399d373eb82f81969a43e7a23f
Bardizba Z <moch.bardizba@gmail.com>
2022-06-03 21:44:25 +0700

    feat: sorter function

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, sorter) => {
+  const sorted = sorter(names)
+  const result = [];
+  for (let i = 0; i < sorted.length; i++) {
+    const name = sorted[i]
+    result.push(`${i+1}. ${name}`)    
+  }
+
+  return result
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => names.sort((a, b) => a < b ? -1 : 1);
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => names.sort((a, b) => a > b ? -1 : 1);
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
